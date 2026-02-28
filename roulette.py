70ac0f54880c72e2ee8f3cc8f276cdeec37b4731
PurpurSlugh <watb.fulminozzo@gmail.com>
2026-02-28 08:25:19 +0100

    Add files via upload

--- roulette.py
+++ roulette.py
@@ -1,62 +1,76 @@
 import random
 from colorama import Fore, Back, Style, init
 
-init()
-money = 1000
+init(autoreset=True)
 
-while True:
-    print(f"\nYou have {money}$")
+balance = 1000
+RED_NUMBERS = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36}
+
+def get_styled_num(n):
+    if n == 0:
+        return f"{Back.WHITE}{Fore.GREEN}  0  {Style.RESET_ALL}"
+    num_color = Fore.RED if n in RED_NUMBERS else Fore.BLACK
+    return f"{Back.WHITE}{num_color}{n:^4}{Style.RESET_ALL}"
+
+
+row1 = range(3, 37, 3)
+row2 = range(2, 36, 3)
+row3 = range(1, 35, 3)
+offset = "      "
+
+while balance > 0:
+   
+    print("\n" + offset + " ".join(get_styled_num(n) for n in row1))
+    print(get_styled_num(0) + " " + " ".join(get_styled_num(n) for n in row2))
+    print(offset + " ".join(get_styled_num(n) for n in row3))
     
-
-    bet_input = input("How much do you want to bet? ")
-    if not bet_input.isdigit():
-        print("Please enter a valid number")
+    print(f"\nBalance: {balance}$")
+    bet_input = input("Bet amount: ")
+    if not bet_input.isdigit(): continue
+    
+    bet_amount = int(bet_input)
+    if bet_amount > balance:
+        print("Insufficient funds!")
         continue
-    cashout = int(bet_input)
-    
-    if cashout > money:
-        print("You don't have enough money!")
-        continue
 
-    print("Please select a number (It's optional)!")
-    print("Select x if don't want to bet on the number!")
-    number = input().lower()
-    
-    print("Please select a color!")
-    color = input().lower()
-    colors = ""
+    selected_number = input("Select a number (0-36) or 'x': ").lower()
+    selected_color = input("Select color (red/black/green): ").lower()
 
-    gamble = random.randint(0, 36)
-    print("\nResult: ", end="")
-    if (gamble == 0):
-        print(Fore.GREEN, gamble, Style.RESET_ALL)
-        colors = "green"
-    elif gamble in [1,3,5,7,9,12,14,16,18,19,21,23,25,27,30,32,34,36]:
-        print(Fore.RED, gamble, Style.RESET_ALL)
-        colors = "red"
+   
+    result = random.randint(0, 36)
+    if result == 0:
+        win_color = "green"
+        display_color = Fore.GREEN
+    elif result in RED_NUMBERS:
+        win_color = "red"
+        display_color = Fore.RED
     else:
-        print(Back.WHITE, Fore.BLACK, gamble, Style.RESET_ALL)
-        colors = "black"
+        win_color = "black"
+        display_color = Fore.BLACK
+
+    print(f"\nResult: {Back.WHITE}{display_color}  {result}  {Style.RESET_ALL}")
+
+ 
+    won_bet = False
+    current_win = 0
+
+  
+    if selected_number.isdigit() and int(selected_number) == result:
+        print(f"STRAIGHT UP! You hit {result}!")
+        current_win += (bet_amount * 35)
+        won_bet = True
 
     
-    won = False
-    
+    if selected_color == win_color:
+        print(f"Color match: {win_color.upper()}!")
+        current_win += (bet_amount * 2)
+        won_bet = True
 
-    if number != "x" and number.isdigit() and int(number) == gamble:
-        print("YOU SELECTED THE CORRECT NUMBER!")
-        money = money + (cashout * 35)
-        won = True
-    
+    if won_bet:
+        balance += current_win
+        print(f"Total win: {current_win}$")
+    else:
+        balance -= bet_amount
+        print(f"Loss: -{bet_amount}$")
 
-    if color == colors:
-        print("You selected the correct color!")
-        money = money + (cashout * 2)
-        won = True
-
-    if not won:
-        print("You lost your bet.")
-        money = money - cashout
-
-    if money <= 0:
-        print("Game Over! You are out of money.")
-        break
+print("Game Over!")
